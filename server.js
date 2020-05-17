bf3cd66f0b8ca699d241f990bc406864b5655bae
Gaurav <gauravzapsingh@gmail.com>
2020-05-17 21:21:48 +0530

    First Commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8000
 
 require('dotenv').config()
 
