2a065b329fb0235029ab2ff23b67e88a93bd95c6
Gaurav <gauravzapsingh@gmail.com>
2020-05-17 22:11:54 +0530

    First Commit updated front-end folder in server

--- server.js
+++ server.js
@@ -40,12 +40,12 @@ mongoose.connect(process.env.MONGODB_URI || localUri, { useNewUrlParser: true, u
 })
 
 if (process.env.NODE_ENV === 'production') {
-    // app.use(express.static('client/build'))
-    app.use(express.static(path.join(__dirname, 'front-end', 'build')));
+    app.use(express.static('front-end/build'))
+    // app.use(express.static(path.join(__dirname, 'front-end', 'build')));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'front-end', 'build', 'index.html'))
-    });
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.join(__dirname, 'front-end', 'build', 'index.html'))
+    // });
 }
 app.listen(PORT, () => {
     console.log(`Server is running on Port ${PORT}`)
